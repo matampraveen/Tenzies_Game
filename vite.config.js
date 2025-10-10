f4db733caa21d02c4169c6c0fd1fcff6e07ca8f1
Praveen <praveenswamymatam7@gmail.com>
2025-10-10 11:24:06 +0530

    Chnaged css for mobile

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  //base: '/Tenzies_Game/',
   plugins: [react()],
+  // REMOVE base if set, or set it to '/'
+  // base: '/', // best to just not set base for Vercel!
 })
-
 
